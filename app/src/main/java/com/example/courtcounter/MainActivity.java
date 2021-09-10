7dc1cfa2e252ec3e59b60668f0fe6e5f91fa559a
timvanderstam <timvanderstam@hotmail.com>
2021-09-10 10:13:37 +0200

    mooi gemaakt

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -17,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         teamAScore =0;
         teamBScore =0;
+        displayForTeamA(teamAScore);
+        displayForTeamB(teamBScore);
 
     }
 
